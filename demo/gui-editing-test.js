399945f655a20cebb1b5cbe84261f6d72740cf3f
N.B <116039108+JakkuAzzo@users.noreply.github.com>
2025-12-04 10:59:52 -0500

    feat: Update REPO_URL to use environment variable and add empty instagram demo file

--- demo/gui-editing-test.js
+++ demo/gui-editing-test.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const REPO_URL = 'https://github.com/JakkuAzzo/GuyRofe.git';
+const REPO_URL = process.env.REPO_URL || 'https://github.com/JakkuAzzo/GuyRofe.git';
 
 async function runGUITest() {
   console.log('🚀 Starting GhP WebEditor GUI Editing Test...\n');
